b87fabaa03db5391e74458778aff9c7c6ccb48b1
VOOleksandrov22 <vooleksandrov22@codingburgas.bg>
2025-07-07 20:18:25 +0300

    Add initial models and basic api endpoints for testing

---
+++ SIMS_BACKEND/SIMS_BACKEND/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace SIMS_BACKEND.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
+
+        public int SchoolId { get; set; }
+        public School School { get; set; } = null!;
+
+    }
+}
---
+++ SIMS_BACKEND/SIMS_BACKEND/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SIMS_BACKEND.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApiContext>(options =>
+   options.UseSqlServer(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter(); // This line now works due to the added namespace
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+   .AddEntityFrameworkStores<ApiContext>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
