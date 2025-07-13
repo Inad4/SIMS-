af337d9c7d524146359299a75deb0171431ac27e
VOOleksandrov22 <vooleksandrov22@codingburgas.bg>
2025-07-13 09:56:30 +0300

    Implement sign up page

--- src/sims_frontend/src/app/auth/login/page.tsx
+++ src/sims_frontend/src/app/auth/login/page.tsx
@@ -14,7 +14,7 @@ export default function LoginPage() {
     let handleLogin = () => {
         setErrorMessage("");
         // Send API request to the backend with email and password
-        let loginSuccessful = email != "error";
+        let loginSuccessful = email != "error@example.com";
         if (!loginSuccessful){
             setErrorMessage("Incorrect login credentials");
             return;
@@ -27,7 +27,7 @@ export default function LoginPage() {
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <Link href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                         <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
-                        Flowbite
+                        SIMS
                     </Link>
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -38,6 +38,7 @@ export default function LoginPage() {
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                                     <input
+                                        type="email"
                                         name="email"
                                         id="email"
                                         className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
